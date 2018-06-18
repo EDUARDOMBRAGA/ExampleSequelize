8acff11c8dd35e34c055cb034e3c3e3900bc381f
Eduardo Mendonça Braga <eduardomendoncabraga@gmail.com>
2018-06-18 09:54:49 -0300

    update index to teste create new user database where sequelize

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { User } = require('./app/models');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false}));
 
+User.create({
+    name: "Eduardo",
+    email: "eduardo@smnti.com.br",
+    password: "123456"
+});
+
 app.get('/', (req, res) => {res.send('Hello World!')});
 
 app.listen(3000);
+
+app.get('/users', (req, res) => { }); //Listar todos
+app.post('/register', async(req, res) => { const user = await User.create(req.body); res.json(user)}); // Criar
+app.get('/users/:id', (req, res) => { }); //Buscar
+app.put('/users/:id', (req, res) => { }); //Editar
+app.delete('/users/:id', (req, res) => { }); //Deletar
